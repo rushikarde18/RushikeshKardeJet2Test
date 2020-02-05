45a8f689e5d7e5299cd76c18cc297219d3690dc9
rushikarde18@gmail.com <rushikarde18@gmail.com>
2020-02-05 15:52:29 +0000

    Initial commit

---
+++ app/src/main/java/com/s/rushikeshkardejet2test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.s.rushikeshkardejet2test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
